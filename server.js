a6d117877c5355dc029866214c1a179804f3de75
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-25 14:28:18 +0000

    update (refactor)

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('MongoDB connected...');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
 app.get('/', (req, res) => {
     res.send('Hello API')
 })
@@ -12,14 +23,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('API is running on port 3000');
 })
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB connected...');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
