b6fd1e3f8abbe27fc60ebf547409f035b0ddb581
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-26 23:17:42 +0000

    update

--- server.js
+++ server.js
@@ -26,11 +26,21 @@ app.get('/', (req, res) => {
 app.get('/products', async(req, res) => {
     try {
         const products = await Product.find({});
-        res.status(200).json(products)
+        res.status(200).json(products);
         
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});   
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
         
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
 })
 
