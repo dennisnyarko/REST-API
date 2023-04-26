68c683625946f9f17188d40dd3a27440ba7d6f6a
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-26 23:55:52 +0000

    update

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 const Product = require('./models/productModel');
 
-
+//middleware for mode of data entry
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -23,54 +25,54 @@ app.get('/', (req, res) => {
     res.send('Hello API')
 })
 
-app.get('/products', async(req, res) => {
+app.get('/products', async (req, res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message});   
+        res.status(500).json({ message: error.message });
     }
 })
 
-app.get('/products/:id', async(req, res) => {
+app.get('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product)
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 })
 
-app.post('/product', async(req, res) => {
+app.post('/product', async (req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product);
-        
+
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
-        
+        res.status(500).json({ message: error.message })
+
     }
 })
 
 
 // update a product
-app.put('/products/:id', async(req, res) => {
+app.put('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
         // when we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!product) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         const updatedProduct = await Product.findById(id)
         res.status(200).json(updatedProduct);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
