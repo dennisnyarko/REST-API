99d93e67fa8f9fee082bd359ec8f176505320a3f
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-27 22:35:37 +0000

    comment

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, {
         console.error('MongoDB connection error:', err);
     });
 
+// read initial message
 app.get('/', (req, res) => {
     res.send('Hello API')
 })
@@ -83,19 +84,19 @@ app.put('/products/:id', async (req, res) => {
 
 
 // delete a product
-app.delete('/products/:id', async(req, res) => {
+app.delete('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!product) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         res.status(200).json(product);
-        
+
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-}) 
+})
 
 // port number
 app.listen(3000, () => {
