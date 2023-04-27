42039d46ca65705be9fafe5440552e530631213d
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-27 00:06:54 +0000

    update

--- server.js
+++ server.js
@@ -77,6 +77,21 @@ app.put('/products/:id', async (req, res) => {
 })
 
 
+// delete a product
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}) 
+
 
 app.listen(3000, () => {
     console.log('API is running on port 3000');
